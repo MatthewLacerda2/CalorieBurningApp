ce0f7bba84e316716660b2f44a31ff6c612ed1b3
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-25 02:35:09 -0300

    useful components

---
+++ calorieburningapp.client/src/Components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <div className="main-header">
+      <div className="logo">
+        FixFlow
+      </div>
+    </div>
+  );
+};
+
+export default Header;
