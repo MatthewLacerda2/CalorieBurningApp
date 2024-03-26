c8f184a9e8b5f3c54b243a02aef7cd9f02a169d0
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-26 19:19:59 -0300

    re-styled register/login

--- calorieburningapp.client/src/Components/MainPage/Register/Register.tsx
+++ calorieburningapp.client/src/Components/MainPage/Register/Register.tsx
@@ -17,9 +17,9 @@ const Register: React.FC<LoginProps> = ({ toggleForm }) => {
           <input className='input-text' style={{ width: '20em' }} type="email" placeholder="Email" />
           <input className='input-text' style={{ width: '20em' }} type="password" placeholder="Password" />
           <p></p>
-          <button className="send-button" style={{ backgroundColor: 'blue' }} onClick={toggleForm}>Register</button>
+          <button className="send-button" style={{ width: '105px', fontSize: '20px' }} onClick={handleLogin}>Register</button>
           <p></p>
-          <button className="send-button" style={{ width: '85px', fontSize: '20px' }} onClick={handleLogin}>Login</button>
+          <button className="send-button" style={{ width: '85px', backgroundColor: 'blue' }} onClick={toggleForm}>Login</button>          
       </div>
     </Card>
   );
