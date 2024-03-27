a21a15c1695c3ee6011d5c7c8cbaf9bae1d7663a
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-27 12:05:41 -0300

    all leaderboarders controllers

--- CalorieBurningApp.Server/Controllers/LeaderboardController.cs
+++ CalorieBurningApp.Server/Controllers/LeaderboardController.cs
@@ -11,12 +11,14 @@ namespace CalorieBurningApp.Server.Controllers;
 [ApiController]
 [Route("api/v1/leaderboard")]
 [Produces("application/json")]
-public class LeaderboardController : ControllerBase{
+public class LeaderboardController : ControllerBase
+{
 
     private readonly ServerContext _context;
     private readonly UserManager<User> _userManager;
 
-    public LeaderboardController(ServerContext context, UserManager<User> userManager){
+    public LeaderboardController(ServerContext context, UserManager<User> userManager)
+    {
         _context = context;
         _userManager = userManager;
     }
@@ -24,30 +26,106 @@ public class LeaderboardController : ControllerBase{
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<UserDTO[]>))]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
     [HttpGet]
-    public async Task<IActionResult> ReadByUsersRanks(int offset, int limit) {
+    public async Task<IActionResult> ReadUsersCaloriesRanks(int offset, int limit)
+    {
 
-        if (offset < 0){
+        if (offset < 0)
+        {
             return BadRequest("Offset parameter must be a natural number greater than 0");
         }
 
-        if (limit < 1) {
+        if (limit < 1)
+        {
             return BadRequest("Limit parameter must be a natural number greater than 0");
         }
 
         var UsersQuery = _context.Users.AsQueryable();
 
-        UsersQuery.OrderBy(u=>u.burnedCalories);
+        UsersQuery.OrderBy(u => u.burnedCalories);
 
         UsersQuery = UsersQuery.Skip(offset);
         UsersQuery = UsersQuery.Take(limit);
 
         var resultQuery = await UsersQuery.ToArrayAsync();
-        var resultsArray = resultQuery.Select(c=>(UserDTO)c).ToArray();
-        
-        if(resultsArray.Length==0){
+        var resultsArray = resultQuery.Select(c => (UserDTO)c).ToArray();
+
+        if (resultsArray.Length == 0)
+        {
             return NotFound("You skipped too many");
         }
-        
+
+        var response = JsonConvert.SerializeObject(resultsArray);
+
+        return Ok(response);
+    }
+
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Streak[]>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    [HttpGet]
+    public async Task<IActionResult> ReadStreakCountRanks(int offset, int limit)
+    {
+
+        if (offset < 0)
+        {
+            return BadRequest("Offset parameter must be a natural number greater than 0");
+        }
+
+        if (limit < 1)
+        {
+            return BadRequest("Limit parameter must be a natural number greater than 0");
+        }
+
+        var UsersQuery = _context.Streaks.AsQueryable();
+
+        UsersQuery.OrderBy(u => u.count);
+
+        UsersQuery = UsersQuery.Skip(offset);
+        UsersQuery = UsersQuery.Take(limit);
+
+        var resultQuery = await UsersQuery.ToArrayAsync();
+        var resultsArray = resultQuery.Select(c => c).ToArray();
+
+        if (resultsArray.Length == 0)
+        {
+            return NotFound("You skipped too many");
+        }
+
+        var response = JsonConvert.SerializeObject(resultsArray);
+
+        return Ok(response);
+    }
+
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Streak[]>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    [HttpGet]
+    public async Task<IActionResult> ReadStreakRecordRanks(int offset, int limit)
+    {
+
+        if (offset < 0)
+        {
+            return BadRequest("Offset parameter must be a natural number greater than 0");
+        }
+
+        if (limit < 1)
+        {
+            return BadRequest("Limit parameter must be a natural number greater than 0");
+        }
+
+        var UsersQuery = _context.Streaks.AsQueryable();
+
+        UsersQuery.OrderBy(u => u.record);
+
+        UsersQuery = UsersQuery.Skip(offset);
+        UsersQuery = UsersQuery.Take(limit);
+
+        var resultQuery = await UsersQuery.ToArrayAsync();
+        var resultsArray = resultQuery.Select(c => c).ToArray();
+
+        if (resultsArray.Length == 0)
+        {
+            return NotFound("You skipped too many");
+        }
+
         var response = JsonConvert.SerializeObject(resultsArray);
 
         return Ok(response);
