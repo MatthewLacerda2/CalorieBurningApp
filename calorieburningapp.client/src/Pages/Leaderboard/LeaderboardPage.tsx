b01f7416a3dc1a9cedf7433754bbf62d59ebc609
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-28 01:21:01 -0300

    leaderboarders page

--- calorieburningapp.client/src/Pages/Leaderboard/LeaderboardPage.tsx
+++ calorieburningapp.client/src/Pages/Leaderboard/LeaderboardPage.tsx
@@ -1,23 +1,38 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { UserDTO } from "../../Data/UserDTO";
+import { Streak } from "../../Data/Streak";
 
 const LeaderboardPage: React.FC = () => {
-  const [streaks, setStreaks] = useState<UserDTO[]>([]);
+  const [burnedCalories, setBurnedCalories] = useState<UserDTO[]>([]);
+  const [currentStreaks, setCurrentStreaks] = useState<Streak[]>([]);
+  const [recordStreaks, setRecordStreaks] = useState<Streak[]>([]);
 
   useEffect(() => {
-    const fetchStreaks = async () => {
+    const fetchLeaderboardData = async () => {
       try {
-        const response = await axios.get(
-          "http://localhost:5071/api/v1/leaderboard?limit=10"
+        const burnedCaloriesResponse = await axios.get(
+          "http://localhost:5071/api/v1/leaderboard/calories/?offset=0&limit=10"
         );
-        setStreaks(response.data);
+        setBurnedCalories(burnedCaloriesResponse.data);
+
+        const currentResponse = await axios.get(
+          "http://localhost:5071/api/v1/leaderboard/streaks/?sort=count&offset=0&limit=10"
+        );
+        console.log("Current Streaks Response:", currentResponse.data); // Log the response data
+        setCurrentStreaks(currentResponse.data);
+
+        const recordResponse = await axios.get(
+          "http://localhost:5071/api/v1/leaderboard/streaks/?sort=record&offset=0&limit=10"
+        );
+        console.log("Record Streaks Response:", recordResponse.data); // Log the response data
+        setRecordStreaks(recordResponse.data);
       } catch (error) {
-        console.error("Error fetching streak records:", error);
+        console.error("Error fetching leaderboard data:", error);
       }
     };
 
-    fetchStreaks();
+    fetchLeaderboardData();
   }, []);
 
   return (
@@ -28,15 +43,69 @@ const LeaderboardPage: React.FC = () => {
           <tr>
             <th>Rank</th>
             <th>UserName</th>
+            <th>FullName</th>
             <th>Burned Calories</th>
           </tr>
         </thead>
         <tbody>
-          {streaks.map((streak, index) => (
-            <tr key={streak.id}>
+          {burnedCalories.map((user, index) => (
+            <tr key={user.id}>
               <td>#{index + 1}</td>
-              <td>{streak.userName}</td>
-              <td>{streak.burnedCalories}</td>
+              <td>{user.userName}</td>
+              <td>{user.fullName}</td>
+              <td>{user.burnedCalories}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <br />
+
+      <h2>Current Streaks Rank</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>UserName</th>
+            <th>Full Name</th>
+            <th>Current Streak</th>
+            <th>Record</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentStreaks.map((streak, index) => (
+            <tr key={streak.UserId}>
+              <td>#{index + 1}</td>
+              <td>{streak.UserName}</td>
+              <td>{streak.FullName}</td>
+              <td>{streak.count}</td>
+              <td>{streak.record}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <br />
+
+      <h2>Record Streaks Ranks</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>UserName</th>
+            <th>Full Name</th>
+            <th>Record</th>
+            <th>Current Streak</th>
+          </tr>
+        </thead>
+        <tbody>
+          {recordStreaks.map((streak, index) => (
+            <tr key={streak.UserId}>
+              <td>#{index + 1}</td>
+              <td>{streak.UserName}</td>
+              <td>{streak.FullName}</td>
+              <td>{streak.record}</td>
+              <td>{streak.count}</td>
             </tr>
           ))}
         </tbody>
