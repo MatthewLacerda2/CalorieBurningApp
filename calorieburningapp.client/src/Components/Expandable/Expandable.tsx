2b6a6e7c0ff2591a5252ad166f1827bd62a95312
Matheus Lacerda <matheus.l1996@gmail.com>
2024-04-05 17:05:16 -0300

    prettier latest entries

--- calorieburningapp.client/src/Components/Expandable/Expandable.tsx
+++ calorieburningapp.client/src/Components/Expandable/Expandable.tsx
@@ -19,13 +19,16 @@ const Expandable: React.FC<ExpandableProps> = ({
   };
 
   return (
-    <div className="expandable-container">
-      <div className="expandable-header" onClick={toggleExpanded}>
+    <div className="expandable-container" style={{ textAlign: "left" }}>
+      <div
+        className="expandable-header"
+        onClick={toggleExpanded}
+        style={{ display: "flex" }}>
         <div className="arrow">{expanded ? "▼" : "▶"}</div>
-        <h2>{headline}</h2>
+        <h2>{headline.charAt(0).toUpperCase() + headline.slice(1)}</h2>
       </div>
       {expanded && (
-        <div className="expandable-content">
+        <div className="expandable-content" style={{ marginLeft: "10px" }}>
           {content.map((item, index) => (
             <p key={index}>{item}</p>
           ))}
